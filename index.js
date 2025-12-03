999c4a8ade70e2d5a1c4b8be74030c687aa601ba
Derek Sesni <230892@utxicotepec.edu.mx>
2025-12-03 10:57:22 -0600

    Configuracion de BD
    Se creo el archivo correspondiente para la conexion de base de datos asi como la implementacion de dotenv para proteger los datos sencibles

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-// Crear el servidor
+
+import { connectDB} from "./src/config/database.js";
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
+//Conexion a la base de datos
+connectDB();
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hola soy el inicio");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
